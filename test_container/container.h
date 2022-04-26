2b67d11d99ff827da9880b1e8acd7d7d5c0a8647
n34prus <n34prus@gmail.com>
2022-04-26 16:22:59 +0400

    двусвязный список с итераторами

--- test_container/container.h
+++ test_container/container.h
@@ -1,9 +1,7 @@
 #pragma once
-template <typename T>
+template <class T>
 class List {
 private:
-
-	template <typename T>
 	//	элементы контейнера
 	class Node {
 	public:
@@ -16,71 +14,13 @@ private:
 			this->pPrev = pPrev;
 		}
 	};
-
-	int size;			//	размер контейнера
-	Node<T> *head;		//	указатель на нулевой элемент
-	Node<T>* tail;		//	указатель на последний элемент
-
-public:
-	
-	List() {
-		size = 0;
-		head = nullptr;
-		tail = nullptr;
-	}
-	~List() {
-		clear();
-	}
-
-	int get_size() { return size; };
-	
-	//	добавление элемента в конец
-	void push_back(T data) {
-		if (size) {
-			Node<T>* temp = tail;
-			tail = new Node<T>(data, nullptr, tail);
-			temp->pNext = tail;
-		}
-		else {
-			tail = new Node<T>(data);
-			head = tail;
-		}
-		size++;
-	}
-	
-	//	добавление элемента в начало
-	void push_front(T data) {
-		if (size) {
-			Node<T>* temp = head;
-			head = new Node<T>(data, head, nullptr);
-			temp->pPrev = head;
-		}
-		else {
-			head = new Node<T>(data);
-			tail = head;
-		}
-		size++;
-	}
-
-	//	удаление первого элемента
-	void pop_front() {
-		Node<T> *temp = head;
-		head = head->pNext;
-		delete temp;
-		size--;
-	}
-
-	//	удаление последнего элемента (однсявязный список)
-	void pop_back() {
-		Node<T>* temp = tail;
-		tail = tail->pPrev;
-		delete temp;
-		size--;
-	}
+	int size;		//	размер контейнера
+	Node* head;		//	указатель на нулевой элемент
+	Node* tail;		//	указатель на последний элемент
 
 	// достаем указатель на элемент по индексу
-	Node<T>* search(int index) {
-		Node<T>* ptr = nullptr;
+	Node* search(int index) {
+		Node* ptr = nullptr;
 		//	проход с головы
 		if (index < (int)(size / 2)) {
 			ptr = this->head;
@@ -98,13 +38,108 @@ public:
 		return ptr;
 	}
 
+public:
+	class Iterator
+	{
+	private:
+		Node* nodePtr;
+
+	public:
+		Iterator() { this->nodePtr = nullptr; }
+		Iterator(Node* nodePtr) { this->nodePtr = nodePtr; }
+
+		Iterator operator++(int) {
+			Iterator it = *this;
+			nodePtr = nodePtr->pNext;
+			return it;
+		}
+		Iterator operator--(int) {
+			Iterator it = *this;
+			nodePtr = nodePtr->pPrev;
+			return it;
+		}
+		Iterator operator+=(const int& shift) {
+			Iterator it = *this;
+			for (int i=0; i<shift; i++)	nodePtr = nodePtr->pNext;
+			return it;
+		}
+		Iterator operator-=(const int& shift) {
+			Iterator it = *this;
+			for (int i = 0; i < shift; i++)	nodePtr = nodePtr->pPrev;
+			return it;
+		}
+		T& operator*() { return nodePtr->data; }
+		bool operator==(const Iterator& it) { return nodePtr->data == it.nodePtr->data; }
+		bool operator!=(const Iterator& it) { return nodePtr->data != it.nodePtr->data; }
+	};
+
+	List() {
+		size = 0;
+		head = nullptr;
+		tail = nullptr;
+	}
+	~List() {
+		clear();
+	}
+
+	Iterator begin() { return Iterator(head); }
+	Iterator last() { return Iterator(tail); }
+	Iterator end() { return Iterator(tail->pNext); }
+
+	//	возвращает размер контейнера
+	int get_size() { return size; };
+	
+	//	добавление элемента в конец
+	void push_back(T data) {
+		if (size) {
+			Node* temp = tail;
+			tail = new Node(data, nullptr, tail);
+			temp->pNext = tail;
+		}
+		else {
+			tail = new Node(data);
+			head = tail;
+		}
+		size++;
+	}
+	
+	//	добавление элемента в начало
+	void push_front(T data) {
+		if (size) {
+			Node* temp = head;
+			head = new Node(data, head, nullptr);
+			temp->pPrev = head;
+		}
+		else {
+			head = new Node(data);
+			tail = head;
+		}
+		size++;
+	}
+
+	//	удаление первого элемента
+	void pop_front() {
+		Node *temp = head;
+		head = head->pNext;
+		delete temp;
+		size--;
+	}
+
+	//	удаление последнего элемента
+	void pop_back() {
+		Node* temp = tail;
+		tail = tail->pPrev;
+		delete temp;
+		size--;
+	}
+
 	//	добавление элемента по индексу
 	void insert(T data, int index) {
 		if (!index) push_front(data);
 		else if (index == size) push_back(data);
 		else{
-			Node<T>* ptr = search(index);
-			Node<T>* newNode = new Node<T>(data, ptr, ptr->pPrev);
+			Node* ptr = search(index);
+			Node* newNode = new Node(data, ptr, ptr->pPrev);
 			ptr->pPrev = newNode;
 			ptr = newNode->pPrev;
 			ptr->pNext = newNode;
@@ -117,8 +152,8 @@ public:
 		if (!index) pop_front();
 		else if (index == size - 1) pop_back();
 		else {
-			Node<T>* ptr = search(index);
-			Node<T>* temp = ptr->pPrev;
+			Node* ptr = search(index);
+			Node* temp = ptr->pPrev;
 			temp->pNext = ptr->pNext;
 			temp = ptr->pNext;
 			temp->pPrev = ptr->pPrev;
@@ -134,148 +169,8 @@ public:
 
 	//	перегрузка [] для обращения по абсолютному индексу
 	T& operator[](const int index) {
-		Node<T>* ptr = search(index);
+		Node* ptr = search(index);
 		return ptr->data;
 	}
-	/*
-	class Iterator {
-	protected:
-		Node<T> *nodePtr;
-	public:
-		explicit Iterator(Node<T>* nodePtr = nullptr);
-		Iterator(const Iterator& Iterator<T>);
-		~Iterator() {
-
-		}
-		/*
-		iterator *begin() {
-			nodePtr = head;
-			return this;
-		};
-		*/
-		//void end();
-		/*
-		T& get() {
-			return *nodePtr->data;
-		}
-		*//*
-	};
-
-	Iterator& operator=(const List<T>::Iterator &*a) {
-		this->nodePtr = a.nodePtr;
-	}
-
-	Iterator<T> begin() { return Iterator<T>(head); }
-	Iterator<T> end() { return Iterator<T>(tail); }	//->pNext?
-
-	Iterator<T> begin() const { return Iterator<T>(head); }
-	Iterator<T> end() const { return Iterator<T>(tail); }
-
-	Iterator<const T> cbegin() const { return head; }
-	Iterator<const T> cend() const { return tail; }*/
-
-	/*
-	//	ранние версии методов
-	//	добавление элемента в конец (односвязный список)
-	void push_back(T data) {
-		if (head == nullptr) head = new Node<T>(data);
-		else {
-			Node<T>* current = this->head;
-			while (current->pNext != nullptr)
-			{
-				current = current->pNext;
-			}
-			current->pNext = new Node<T>(data);
-		}
-		size++;
-	};
-
-	//	добавление элемента в начало (односвязный список)
-	void push_front(T data) {
-		head = new Node<T>(data, head);
-		if (!size) tail = head;
-		size++;
-	};
-
-	//	удаление последнего элемента (однсявязный список)
-	void pop_back() {
-		remove(size - 1);
-	}
-
-	//	добавление элемента по индексу
-	void insert(T data, int index) {
-		if (!index) push_front(data);
-		else if (index == size) push_back(data);
-		//	проход с головы
-		else if (index <= (int)(size/2)) {
-			Node<T> *ptr = this->head;
-			for (int i = 0; i < index - 1; i++) {
-				ptr = ptr->pNext;
-			}
-			//записываем в новый элемент указатели на соседние элементы
-			Node<T> *newNode = new Node<T>(data, ptr->pNext, ptr);
-			//записываем в предыдущий элемент указатель на новый элемент
-			ptr->pNext = newNode;
-			//записываем в следующий элемент указатель на новый элемент
-			ptr = newNode->pNext;
-			ptr->pPrev = newNode;
-			size++;
-		}
-		//	проход с хвоста
-		else {
-			Node<T>* ptr = this->tail;
-			for (int i = size-1; i > index; i--) {
-				ptr = ptr->pPrev;
-			}
-			Node<T>* newNode = new Node<T>(data, ptr, ptr->pPrev);
-			ptr->pPrev = newNode;
-			ptr = newNode->pPrev;
-			ptr->pNext = newNode;
-			size++;
-		}
-	}
-
-	//	удаление элемента по индексу (односвязный список)
-	void remove(int index) {
-		if (!index) pop_front;
-		else if (index == size - 1) pop_back();
-		else {
-			Node<T> *ptr = this->head;
-			for (int i = 0; i < index - 1; i++) {
-				ptr = ptr->pNext;
-			}
-			Node<T>* temp = ptr->pNext;
-			ptr->pNext = temp->pNext;
-			delete temp;
-			size--;
-		}
-	}
-
-	//	перегрузка [] для обращения по абсолютному индексу
-	T& operator[](const int index) {
-		int counter;
-		if (index <= (int)(size / 2)) {
-			counter = 0;
-			Node<T>* ptr = this->head;
-			while (ptr != nullptr)
-			{
-				if (counter == index) return ptr->data;
-				ptr = ptr->pNext;
-				counter++;
-			}
-		}
-		else {
-			counter = size-1;
-			Node<T>* ptr = this->tail;
-			while (ptr != nullptr)
-			{
-				if (counter == index) return ptr->data;
-				ptr = ptr->pPrev;
-				counter--;
-			}
-		}
-	}
-
-	*/
 
 };
--- test_container/main.cpp
+++ test_container/main.cpp
@@ -1,4 +1,7 @@
-﻿#include <iostream>
+﻿//  демонстрация работы методов контейнера
+//  Каргин М.С.
+
+#include <iostream>
 #include "container.h" 
 
 using namespace std;
@@ -6,37 +9,57 @@ List<int> list;
 
 void print() {
     for (int i = 0; i < list.get_size(); i++) cout << list[i] << " ";
-    cout << endl;
+    cout << endl << endl;
+};
+void print(string s) {
+    cout << s << endl;
+    for (int i = 0; i < list.get_size(); i++) cout << list[i] << " ";
+    cout << endl << endl;
 };
 
 int main()
 {
-    list.push_front(2);
-    print();
+    srand(time(NULL));
 
-    list.push_front(1);
-    print();
+    //  работа по индексу
+    //  добавление элементов
+    for (int i = 0; i < 5; i++) {
+        list.push_front(rand()%10);
+        list.push_back(rand() % 10);
+    }
+    print("push_front & push_back:");
 
-    list.push_back(3);
-    print();
+    //  удаление первого и последнего
+    list.pop_back();
+    list.pop_front();
+    print("pop_front & pop_back:");
 
-    list.push_back(4);
-    print();
+    //  вставка и удаление по индексу
+    list.insert(777, 1);
+    list.remove(list.get_size()-2);
+    print("insert second & remove penult:");
 
-    list.push_back(5);
-    print();
+    //  работа по указателю
+    List<int>::Iterator it;
+    cout << "iterators work:" << endl;
 
-    //list.pop_back();
-    //print();
+    //  сдвигаем вперед от нулевого элемента на 3
+    it = list.begin();
+    it+=2;
+    it++;
+    cout << *it << " | ";
 
-    list.insert(556, 1);
-    print();
+    //  сдвигаем назад от последнего элемента на 3 (list.end() указывает на пустую область!)
+    it = list.last();
+    it -= 2;
+    it--;
+    cout << *it << endl;
 
-    list.remove(4);
-    print();
+    //  меняем текущий элемент и выводим все данные
+    *it = 777;
+    for (List<int>::Iterator i = list.begin(); i != list.end(); i++) {
+        cout << *i << " ";
+    }        
 
-    //List<int>::Iterator it;
-    //it = list.begin();
 
-    //list.pop_front();
 }
