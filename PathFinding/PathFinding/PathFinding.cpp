cbcf56b0710a690c2f8905d86ca615fce17706cb
Mihail <n34prus@gmail.com>
2026-01-23 16:18:09 +0500

    leakage fix
    while scope in main instead of goto

--- PathFinding/PathFinding/PathFinding.cpp
+++ PathFinding/PathFinding/PathFinding.cpp
@@ -36,67 +36,64 @@ int main()
 	long long calcTime = 0;
 	long long totalPathNodes = 0;
 	
-	// restart point FOR DEBUG ONLY
-restart:
+	while (true)
+	{
 
-	// boolean matrix with random noise inside
-	ui.print(	"Call #", ++calls, "\n", "Generate random BitMatrix...\n");
-	BitMatrix source{SIZEX, SIZEY, true};
-	//
-	ui.print("Random BitMatrix generating done\n");
-	
-	// create matrix of Nodes
-	ui.print("Prepare graph from source BitMatrix...\n");
-	auto startTime = std::chrono::system_clock::now();
+		// boolean matrix with random noise inside
+		ui.print("Call #", ++calls, "\n", "Generate random BitMatrix...\n");
+		BitMatrix source{ SIZEX, SIZEY, true };
+		//
+		ui.print("Random BitMatrix generating done\n");
 
-	auto printProgress = [](int progress)
-		{
-			std::cout << "\r" << progress << "%";
-		};
-	Cluster grid(source, printProgress);
+		// create matrix of Nodes
+		ui.print("Prepare graph from source BitMatrix...\n");
+		auto startTime = std::chrono::system_clock::now();
 
-	auto elapsedTime = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime).count();
-	genTime += elapsedTime;
-	ui.print("\rGraph matrix generating done in ", elapsedTime, " ms\n");
+		auto printProgress = [](int progress)
+			{
+				std::cout << "\r" << progress << "%";
+			};
+		Cluster grid(source, printProgress);
 
-	// single-instanced variant works on original matrix
-	/*
-	Node* sourceNode = grid.getRandomNode();
-	Node* targetNode = grid.getRandomNode();
-	std::cout << "Start finding path from " << sourceNode << " to " << targetNode << std::endl; 
-	Dijkstra dijkstra{ grid, sourceNode, targetNode };
-	*/
+		auto elapsedTime = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime).count();
+		genTime += elapsedTime;
+		ui.print("\rGraph matrix generating done in ", elapsedTime, " ms\n");
 
-	// multiple-instanced variant works on matrix copy
-	Dijkstra dijkstra{ grid, grid.getRandomNodeIdx(), grid.getRandomNodeIdx() };
-	dijkstra.bIncludeDiagonal = INCLUDE_DIAGONAL;
-	dijkstra.bExpensiveDiagonal = EXPENSIVE_DIAGONAL;
-	dijkstra.Calculate(printProgress);
+		// single-instanced variant works on original matrix
+		/*
+		Node* sourceNode = grid.getRandomNode();
+		Node* targetNode = grid.getRandomNode();
+		std::cout << "Start finding path from " << sourceNode << " to " << targetNode << std::endl;
+		Dijkstra dijkstra{ grid, sourceNode, targetNode };
+		*/
 
-	elapsedTime = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime).count() - elapsedTime;
-	calcTime += elapsedTime;
-	totalPathNodes += dijkstra.resultPath.size();
+		// multiple-instanced variant works on matrix copy
+		Dijkstra dijkstra{ grid, grid.getRandomNodeIdx(), grid.getRandomNodeIdx() };
+		dijkstra.bIncludeDiagonal = INCLUDE_DIAGONAL;
+		dijkstra.bExpensiveDiagonal = EXPENSIVE_DIAGONAL;
+		dijkstra.Calculate(printProgress);
 
-	// prints statistics
-	ui.print("\r", dijkstra.iterations, " iterations done in ", elapsedTime, " ms\n");
-	if (!dijkstra.resultPath.empty()) ui.print("Path found and contains ", dijkstra.resultPath.size(), " nodes :)\n");
-	else ui.print("Path not found :(\n");
-	ui.print("Generating time ", genTime / calls, " ms per call | ",
-		static_cast<float>(genTime * 1000) / (calls * SIZEX * SIZEY), " us per node\n",
-		"Calculating time ", calcTime / calls, " ms per call | ");
-	if (totalPathNodes) ui.print(static_cast<float>(calcTime) / (totalPathNodes), " ms per path node\n");
+		elapsedTime = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime).count() - elapsedTime;
+		calcTime += elapsedTime;
+		totalPathNodes += dijkstra.resultPath.size();
 
-	// dijkstra: console result visualization
-	if (ENABLE_GRAPHIC) dijkstra.Draw(hwnd, 10);
+		// prints statistics
+		ui.print("\r", dijkstra.iterations, " iterations done in ", elapsedTime, " ms\n");
+		if (!dijkstra.resultPath.empty()) ui.print("Path found and contains ", dijkstra.resultPath.size(), " nodes :)\n");
+		else ui.print("Path not found :(\n");
+		ui.print("Generating time ", genTime / calls, " ms per call | ",
+			static_cast<float>(genTime * 1000) / (calls * SIZEX * SIZEY), " us per node\n",
+			"Calculating time ", calcTime / calls, " ms per call | ");
+		if (totalPathNodes) ui.print(static_cast<float>(calcTime) / (totalPathNodes), " ms per path node\n");
 
-	ui.print("Press any key to start again");
-	while (!_kbhit()) { Sleep(50); }
-	_getch();
-	ui.clear();
-	// DEADLY bad way, only for debug, sorry for that
+		// dijkstra: console result visualization
+		if (ENABLE_GRAPHIC) dijkstra.Draw(hwnd, 10);
 
-	goto restart;
-
+		ui.print("Press any key to start again");
+		while (!_kbhit()) { Sleep(50); }
+		_getch();
+		ui.clear();
+	}
 
     return 0;
 }
